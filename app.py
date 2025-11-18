62bea1ec524e262a91fa3c14af802d3d073b79c7
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2025-11-18 18:39:50 +0100

    Added procfile and requirements.txt

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def clear_conversation():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run("0.0.0.0",port=port,debug=True)
